20b190d6b5745e81d13d8815a156a366dd405d05
Daniel Sarinana <Daniel_E_Sarinana@progressive.com>
2022-10-17 15:05:40 -0400

    Before Break

---
+++ DevelopersSolution/DevelopersApi/Models/DeveloperModels.cs
@@ -0,0 +1,14 @@
+﻿namespace DevelopersApi.Models;
+
+
+
+public record DeveloperDetailsModel(string Id, string FirstName, string LastName, string Phone, string EMail);
+
+public record DeveloperSummaryModel(string Id, string FirstName, string LastName, string EMail);
+
+public class CollectionResponse<T>
+{
+    public List<DeveloperSummaryModel> Data { get; set; }
+}
+
+public record DeveloperCreateModel(string FirstName, string LastName, string Email, string Phone);
